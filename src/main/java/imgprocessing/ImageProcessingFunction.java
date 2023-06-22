3a8c5054e4270227a319b3cb3db32bbd595e5bdb
Martins Greizis <grei0003@hz.nl>
2023-06-22 20:24:07 +0200

    GET req now responds with image

--- src/main/java/imgprocessing/ImageProcessingFunction.java
+++ src/main/java/imgprocessing/ImageProcessingFunction.java
@@ -1,24 +1,44 @@
 package imgprocessing;
 
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobClientBuilder;
+import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Optional;
+
 import javax.imageio.ImageIO;
 
 public class ImageProcessingFunction {
+    Dotenv dotenv = Dotenv.configure().load();
+
+    String connectionString = dotenv.get("STORAGE_CONNECTION_STRING");
+    String containerName = dotenv.get("STORAGE_CONTAINER_NAME");
+
     @FunctionName("ImageProcessingFunction")
     public HttpResponseMessage run(
             @HttpTrigger(name = "req", methods = {
-                    HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<ByteArrayOutputStream> request,
+                    HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) throws Exception {
 
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Retrieve the image from the request
-        byte[] imageBytes = request.getBody().toByteArray();
+        String blobUrl = "https://imgprocessing28e51d5.blob.core.windows.net/images/6ydirw2ugyb61.jpg";
+
+        // Download the image from Azure Blob Storage
+        byte[] imageBytes = downloadImageFromBlobStorage(blobUrl);
+
+        // Read the downloaded image
         ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
         BufferedImage image = ImageIO.read(bis);
 
@@ -35,6 +55,29 @@ public class ImageProcessingFunction {
                 .body(processedImageBytes)
                 .header("Content-Type", "image/jpeg")
                 .build();
+    }
+
+    private byte[] downloadImageFromBlobStorage(String blobUrl) {
+        try {
+            URL url = new URL(blobUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            try (InputStream in = connection.getInputStream()) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    bos.write(buffer, 0, bytesRead);
+                }
+            }
+
+            return bos.toByteArray();
+        } catch (IOException e) {
+            // Handle the exception
+        }
+
+        return null;
     }
 
     private BufferedImage processImage(BufferedImage image) {
